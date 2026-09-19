54e09ec1c9bb47120941d9382d48c7a5e6be8827
kadeejahi <klabaran@luc.edu>
2026-09-19 14:37:21 -0500

    I did my first test, which was with the Linked List. I noted that there didnt seem to be much of a difference in the text file.

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -22,6 +22,7 @@ public class TestIterator {
   public void setUp() throws Exception {
     list = new ArrayList<Integer>();
     // TODO Question: Also try with a LinkedList - does it make any difference?
+// list = new LinkedList<Integer>();
   }
 
   @AfterEach
